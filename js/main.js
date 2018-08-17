91787492ce6e83ea9dd7c0e2d0ec614e2ccfbfad
Fabrizio Marras <fmarras79@gmail.com>
2018-08-17 22:15:47 +0200

    Camera Autorotation enabled and js comments updated.

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function init() {
 // **********************************
 // CREATE 01 - Scene
   var scene = new THREE.Scene(); // create the scene.
-  var gui = new dat.GUI(); // Initiate GUI - controllers on the browser window.
+
 
 // 3D Objects
 // 3D Logo
@@ -23,8 +23,8 @@ function init() {
     objLoader.setMaterials(materials);
     objLoader.setPath('../_assets/');
     objLoader.load('FM_3DLogo.obj', function(object) {
-      object.position.y += 200; // brings up the imported 3D object.
-      scene.add(object);
+      object.position.y += 500; // brings up the imported 3D object.
+      scene.add(object); // add the 3D obj file to the scene.
     }); // OBJ Loader
   }); // MTL Loader
 
@@ -34,69 +34,45 @@ function init() {
   var pointLight02 = getPointLight02(1); // calling the getPointLight function and giving an intensity of 1.
 
 // **********************************
-// PARAMETERS
+// LIGHTS
 // **********************************
-
 // POINTLIGHT 01.
   pointLight01.position.x = 1500;
   pointLight01.position.y = -1500;
-  pointLight01.position.z = -500;
+  pointLight01.position.z = -800;
   pointLight01.intensity = 3;
-
 // POINTLIGHT 01.
   pointLight02.position.x = -1500;
   pointLight02.position.y = 1500;
   pointLight02.position.z = 800;
   pointLight02.intensity = 3;
 
-// adding the back light controllers on screen:
 
-  var folder1 = gui.addFolder('Back Light');
-  folder1.add(pointLight01, 'intensity', 2, 4);
-  folder1.add(pointLight01.position, 'x', 0, 1500);
-  folder1.add(pointLight01.position, 'y', -1500, 1500);
-  folder1.add(pointLight01.position, 'z', -1500, 1500);
-
-// adding the front light controllers on screen:
-  var folder2 = gui.addFolder('Front Light');
-  folder2.add(pointLight02, 'intensity', 2, 4);
-  folder2.add(pointLight02.position, 'x', 0, 1500);
-  folder2.add(pointLight02.position, 'y', -1500, 1500);
-  folder2.add(pointLight02.position, 'z', -1500, 1500);
-
-// ********************************************
-// ADDING TO THE SCENE
-// ********************************************
+// adding the lights to the scene
   scene.add( pointLight01 );  // add the light to the scene.
   scene.add( pointLight02 );  // add the light to the scene.
 
-// CREATE 02 - a CAMERA
+// CREATE 02 - CAMERA
   var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
-
   // camera.position.x = 1;
-  // camera.position.y = 2;
+  camera.position.y = 250;
   camera.position.z = 1500;
-
   camera.lookAt(new THREE.Vector3(0, 0, 0)); // the object is centered in the middle of the screen.
 
 // CREATE 03 - the RENDERER
   var renderer = new THREE.WebGLRenderer();
   // ADD SHADOWS 01 - enabling the shadows in the RENDERER
   renderer.shadowMap.enabled = true;
-  // In addition to creating the renderer instance, we also need to set the size at which we want it to render our app. It's a good idea to use the width and height of the area we want to fill with our app:
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setSize( window.innerWidth, window.innerHeight );// set the size of the 3D environment.
+  renderer.setClearColor(0x222222);  // set the background color.
 
-  renderer.setClearColor(0x222222);  // set the background color to #222222 in between eiphens if css rules are used.
-
-  // add the renderer element to our HTML document. This is a <canvas> element the renderer uses to display the scene to us.
-  document.getElementById('webgl').appendChild(renderer.domElement); // appending the renderer to the div.
+  document.getElementById('webgl').appendChild(renderer.domElement); // add the renderer element to our HTML document.
 
 // ORBIT CONTROL - to orbit around the object with the camera by dragging or clicking.
   var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-  update(renderer, scene, camera, controls); // call the function update which allow to continuously render the scene without the need to reload the page - adding also the controls to the update function will ensure the screen to update after each move.
-
-}; // init function
+  update(renderer, scene, camera, controls); // continuously render the scene
+}; // init function - make everything happen
 
 // **********************************
 // CREATING OBJECTS as FUNCTIONS
@@ -132,8 +108,9 @@ function init() {
     renderer.render( scene, camera ); // renderer
 
     controls.update(); // UPDATE METHOD applied to the controls in order to be able to control the camera with dragging.
-
+    controls.autoRotate = true; // rotate automatically the camera around the object.
     requestAnimationFrame(function() {
+
       update(renderer, scene, camera, controls);// calling back the animation function.
     }); // requestAnimationFrame is a method on the window object -  It's similar to the set interval in a sense that it periodically calls the given function, but it also performs sorts and performs optimizations, regarding when a frame gets painted, which makes it preferable to use this function over set interval function when working with animations.
   };
@@ -141,6 +118,5 @@ function init() {
 // **********************************
 // Callingthe INIT FUNCTION TO RUN ALL
 // **********************************
-
 // Call the initialisation function with all the THREE JS material in it.
 init();
